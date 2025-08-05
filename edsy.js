9067c04c7847b8808257b9d32fd0a0c9928218aa
TalosG97 <geraldig1997@gmail.com>
2025-08-05 13:19:35 +0300

    Update edsy.js

--- edsy.js
+++ edsy.js
@@ -1,3 +1,15 @@
+
+// HARD DISABLE VERSION CHECK LOGIC FOR GITHUB DEPLOYMENT
+try {
+  window._edsy_skipUpdateCheck = true;
+  localStorage.setItem("vhtml", "100000000");
+  localStorage.setItem("vjs", "100000000");
+  localStorage.setItem("vcss", "100000000");
+  localStorage.setItem("vdb", "100000000");
+  localStorage.setItem("last_modified", "20250805");
+} catch (e) {}
+
+
 /*
 EDSY was created using assets and imagery from Elite Dangerous, with the permission of Frontier Developments plc, for non-commercial purposes.
 It is not endorsed by nor reflects the views or opinions of Frontier Developments and no employee of Frontier Developments was involved in the making of it.
