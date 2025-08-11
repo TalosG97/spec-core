5df751feb0bbc53b45f5cb804746a6acc7814324
TalosG97 <geraldig1997@gmail.com>
2025-08-11 12:30:34 +0300

    Update eddb.js

--- eddb.js
+++ eddb.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 EDSY was created using assets and imagery from Elite Dangerous, with the permission of Frontier Developments plc, for non-commercial purposes.
 It is not endorsed by nor reflects the views or opinions of Frontier Developments and no employee of Frontier Developments was involved in the making of it.
 
